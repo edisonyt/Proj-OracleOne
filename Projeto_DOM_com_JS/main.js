2c551aeef7dcfb874b1a198580108b9c9d0dbf15
Edison Takaesu <edisonyt@gmail.com>
2022-07-29 16:41:19 -0300

    modificando aplicativo para receber data e horário, no site cdnjs.com, buscando a biblioteca moment

--- Projeto_DOM_com_JS/main.js
+++ Projeto_DOM_com_JS/main.js
@@ -9,6 +9,10 @@ import BotaoDeleta from './componentes/deletaTarefa.js'
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
+    const calendario = document.querySelector('[data-date]')
+    const data = moment(calendario.value)
+    console.log(data.format('DD/MM/YYYY'))
+
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
 
