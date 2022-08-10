4436ac680f7ca02886d1409ee94196f32d7f7d37
Edison <edisonyt@gmail.com>
2022-08-09 22:21:52 -0300

    node-modules

--- Projeto Nodejs/service/cliente-service.js
+++ Projeto Nodejs/service/cliente-service.js
@@ -1,3 +1,15 @@
+const criaNovaLinha = () => {
+    const conteudo = `
+<td class="td" data-td>${nome}</td>
+<td>${email}</td>
+<td>
+    <ul class="tabela__botoes-controle">
+        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>
+</td> `
+}
+
 const http = new XMLHttpRequest()
 
 http.open('GET','http://localhost:3000/profile')
@@ -6,5 +18,5 @@ http.send()
 
 http.onload = () => {
     const data = http.response
-    console.log(object)
+    console.log(data)
 }
