ad4eeb2dc89c325b9af10761aa6fe53e4eea7b17
Edison <edisonyt@gmail.com>
2022-08-13 21:01:48 -0300

    request encadeada

--- Projeto Nodejs/service/cliente-service.js
+++ Projeto Nodejs/service/cliente-service.js
@@ -1,14 +1,24 @@
-const criaNovaLinha = () => {
+const criaNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr')
     const conteudo = `
-<td class="td" data-td>${nome}</td>
-<td>${email}</td>
-<td>
-    <ul class="tabela__botoes-controle">
-        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-    </ul>
-</td> `
+        <td class="td" data-td>${nome}</td>
+            <td>${email}</td>
+            <td>
+                <ul class="tabela__botoes-controle">
+                    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                </ul>
+            </td>
+        `
+
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
 }
+
+const tabela = document.querySelector('[data-tabela]')
+
+//tabela.appendChild(criaNovaLinha(nome,email))
+
 
 const http = new XMLHttpRequest()
 
@@ -17,6 +27,9 @@ http.open('GET','http://localhost:3000/profile')
 http.send()
 
 http.onload = () => {
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    //console.log(data)
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))    
+    });
 }
